78230a8c4f9e6a847a7ab1cb28e0b9f140ec4700
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-18 04:11:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function increaseRankBy(n) {
 }
 
 function deepestChild() {
-  const node = document.getElementById("grand-node")
-  const nextNode= node.children[0]
+  let node = document.getElementById("grand-node")
+  let nextNode= node.children[0]
   
  while (nextNode) {
    node = nextNode
