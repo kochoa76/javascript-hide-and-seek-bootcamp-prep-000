c3010c0465bc6e6b096027abfa788949b8808569
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-18 04:00:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ function nestedTarget() {
 
 function increaseRankBy(n) {
   const rankedList = document.querySelectorAll(".ranked-list")
-  for (let i= 0; l = rankedList.length; i < l; i++) {
+  for (let i= 0, l = rankedList.length; i < l; i++) {
   let children = rankedList[i].children
   
-  for (let j=0; k= children.length; j < k; j++) {
+  for (let j=0, k= children.length; j < k; j++) {
     children[j].innerHTML = parseInt(children[j].innerHTML) + n
   }
 }
