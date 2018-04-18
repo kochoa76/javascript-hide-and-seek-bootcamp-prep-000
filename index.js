5a1212ded1ae46061f065adc49c6d9deba53332f
ochoa.kayla@yahoo.com <ochoa.kayla@yahoo.com>
2018-04-18 03:55:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n) {
   for (let i= 0; l = rankedList.length; i < l; i++) {
   let children = rankedList[i].children
   
-  for (j=0; k= children.length; j<k; j++) {
+  for (let j=0; k= children.length; j < k; j++) {
     children[j].innerHTML = parseInt(children[j].innerHTML) + n
   }
 }
